43b72eca251f713a2afa30837fd2e65f53f90b4d
Milind_77 <milinddhote77@gmail.com>
2021-05-11 19:30:44 +0530

    Added: UC-5_Compute_Four

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -1,1 +1,20 @@
- 
+#!/bin/bash
+echo "------------------------------------------"
+echo "Welcome To Arithmetic Computation Program"
+echo "------------------------------------------"
+read -p " Enter Number (a): " a
+read -p " Enter Number (b): " b
+read -p " Enter Number (c): " c
+echo "------------------------------------------"
+#UserCase_2
+echo " (a + b * c) =" $(( $a + $b * $c))
+echo "------------------------------------------"
+#UserCase_3
+echo " (a * b + c) =" $(( $a * $b + $c))
+echo "------------------------------------------"
+#UserCase_4
+echo " (c + a + b) =" $(( $c + $a + $b)) 
+echo "------------------------------------------" 
+#UserCase_5
+echo " (a % b + c) =" $(( $a % $b + $c))
+echo "------------------------------------------"
