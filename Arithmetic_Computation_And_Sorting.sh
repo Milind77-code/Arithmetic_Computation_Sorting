8afb1f47c4faea939fe89a24e2a64d5b734c34cb
Milind_77 <milinddhote77@gmail.com>
2021-05-11 19:14:44 +0530

    Added: UC-3_Compute_Two

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -4,8 +4,8 @@ read -p " Enter Number (a): " a
 read -p " Enter Number (b): " b
 read -p " Enter Number (c): " c
 echo "-------------------------------------"
-echo " ($a + $b * $c) =" $(( $a + $b * $c))
+echo " (a + b * c) =" $(( $a + $b * $c))
 echo "-------------------------------------"
 #UserCase_3
-echo " ($a * $b + $c) =" $(( $a * $b + $c)) 
+echo " (a * b + c) =" $(( $a * $b + $c))
 echo "-------------------------------------"
