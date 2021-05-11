86baea86e52c5bbea3583169d0861725bd90f743
Milind_77 <milinddhote77@gmail.com>
2021-05-11 19:36:00 +0530

    Added: UC-1_Take_Three_Inputs

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #UserCase_1
+echo "------------------------------------------"
+echo "Welcome To Arithmetic Computation Program "
+echo "------------------------------------------"
 read -p " Enter Number (a) : " a
 read -p " Enter Number (b) : " b
 read -p " Enter Number (c) : " c 
+echo "------------------------------------------"
