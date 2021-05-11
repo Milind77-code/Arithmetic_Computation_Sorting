395a00df4e93828db9727b006e905e4b88f32008
Milind_77 <milinddhote77@gmail.com>
2021-05-11 19:20:38 +0530

    Added: UC-4_Compute_Three

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -1,1 +1,14 @@
- 
+#!/bin/bash
+read -p " Enter Number (a): " a
+read -p " Enter Number (b): " b
+read -p " Enter Number (c): " c
+echo "-------------------------------------"
+#UserCase_2
+echo " (a + b * c) =" $(( $a + $b * $c))
+echo "-------------------------------------"
+#UserCase_3
+echo " (a * b + c) =" $(( $a * $b + $c))
+echo "-------------------------------------"
+#UserCase_4
+echo " (c + a + b) =" $(( $c + $a + $b)) 
+echo "-------------------------------------"
