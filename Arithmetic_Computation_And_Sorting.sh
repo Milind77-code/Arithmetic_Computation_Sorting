4d5f94a8a26f1d6cae8391b0691fc1035b15cd01
Milind_77 <milinddhote77@gmail.com>
2021-05-11 18:46:07 +0530

    Added: UC-2_Compute_One

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -1,1 +1,9 @@
- 
+#!/bin/bash
+read -p " Enter Number (a): " a
+read -p " Enter Number (b): " b
+read -p " Enter Number (c): " c
+
+#UserCase_2
+echo "-------------------------------------"
+echo " (a + b * c) = " $(( $a + $b * $c)) 
+echo "-------------------------------------"
