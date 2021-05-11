38cda3a1e751dd0c040cb1b1a043cc227ebf9ddf
Milind_77 <milinddhote77@gmail.com>
2021-05-11 20:40:33 +0530

    Added: UC-7_Reading_Values_From_Dictionary_Into_Array

--- Arithmetic_Computation_And_Sorting.sh
+++ Arithmetic_Computation_And_Sorting.sh
@@ -1,1 +1,27 @@
- 
+#!/bin/bash
+echo "------------------------------------------"
+echo "Welcome To Arithmetic Computation Program"
+echo "------------------------------------------"
+read -p " Enter Number (a): " a
+read -p " Enter Number (b): " b
+read -p " Enter Number (c): " c
+echo "------------------------------------------"
+declare -A computeArr
+#UserCase_2
+echo " (a + b * c) =" $(( $a + $b * $c))
+echo "------------------------------------------"
+#UserCase_3
+echo " (a * b + c) =" $(( $a * $b + $c))
+echo "------------------------------------------"
+#UserCase_4
+echo " (c + a + b) =" $(( $c + $a + $b)) 
+echo "------------------------------------------" 
+#UserCase_5
+echo " (a % b + c) =" $(( $a % $b + $c))
+echo "------------------------------------------" 
+#UserCase_7
+numArray=()
+	for number in ${computeArr[@]}
+	do
+		numArray[$number]=$computeArr[$number]
+	done
